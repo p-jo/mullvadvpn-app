ad08001c9bea36d7a5e1404088a19de109f756fc
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-11 19:04:57 +0000

    Stop editing when focus is lost

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
@@ -1,13 +1,22 @@
 package net.mullvad.mullvadvpn.ui.customdns
 
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import android.widget.EditText
 import java.net.InetAddress
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
 
 class EditCustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomDnsItemHolder(view) {
-    private val input: EditText = view.findViewById(R.id.input)
+    private val input: EditText = view.findViewById<EditText>(R.id.input).apply {
+        onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                serverAddress?.let { address ->
+                    adapter.stopEditing(address)
+                }
+            }
+        }
+    }
 
     var serverAddress by observable<InetAddress?>(null) { _, _, hostNameAndAddressText ->
         if (hostNameAndAddressText != null) {
