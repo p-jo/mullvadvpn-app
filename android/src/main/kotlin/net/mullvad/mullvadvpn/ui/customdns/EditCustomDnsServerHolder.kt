14283586edc40e9eccc658748857ab6707335b52
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-11-11 18:14:22 +0000

    Focus on DNS server text when starting to edit

--- android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/ui/customdns/EditCustomDnsServerHolder.kt
@@ -1,23 +1,26 @@
 package net.mullvad.mullvadvpn.ui.customdns
 
 import android.view.View
-import android.widget.TextView
+import android.widget.EditText
 import java.net.InetAddress
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.R
 
 class EditCustomDnsServerHolder(view: View, adapter: CustomDnsAdapter) : CustomDnsItemHolder(view) {
-    private val input: TextView = view.findViewById(R.id.input)
+    private val input: EditText = view.findViewById(R.id.input)
 
     var serverAddress by observable<InetAddress?>(null) { _, _, hostNameAndAddressText ->
         if (hostNameAndAddressText != null) {
             val hostNameAndAddress = hostNameAndAddressText.toString().split('/', limit = 2)
             val address = hostNameAndAddress[1]
 
-            input.text = address
+            input.setText(address)
+            input.setSelection(address.length)
         } else {
-            input.text = ""
+            input.setText("")
         }
+
+        input.requestFocus()
     }
 
     init {
